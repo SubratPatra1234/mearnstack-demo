ca18445604a500f979b3b38186acb14636231de3
SubratPatra1234 <subratpatra392@gmail.com>
2024-10-07 11:17:10 +0530

    Add files via upload

---
+++ mearn.js
@@ -0,0 +1,36 @@
+// var name="pramod";
+// let age =24;
+// const city="bhubaneswar"
+// console.log(address);
+// var address="bhubaneswar";
+// console.log(address);
+
+// let user;
+// user=null;
+// console.log(user);
+//undefined
+let data;
+console.log(typeof data);
+console.log(data);
+//number
+let num1=50;
+let decimal_no=1.001;
+let pi=3.14;
+let age=25;
+let roll_no=135;
+console.log(typeof num1);
+
+//addition
+let num2=52;
+let num3=65;
+let addition=num2+num3;
+console.log(addition);
+//substraction
+let sub=num2-num3;
+console.log(sub);
+//multiplication
+let mul=num2*num3;
+console.log(mul);
+//division
+let div=num2/num3;
+console.log(div);
